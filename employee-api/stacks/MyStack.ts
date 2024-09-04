fdd385a1120b384f7cebcc64bce8f30e68f40fa0
Burak Karlitepe <bkarlitepe@foxbase.de>
2024-09-04 22:33:08 +0200

    feat: add needed resources
    Create Table to store Employees and Import Reports
    Create Queue to handle the Employee Storing and Report Storing
    Create Api Endpoints to start the upload and retrieve the report

--- employee-api/stacks/MyStack.ts
+++ employee-api/stacks/MyStack.ts
@@ -1,7 +1,68 @@
-import { StackContext, Api } from "sst/constructs";
+import { StackContext, Api, Queue, Table } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
-  const api = new Api(stack, "api");
+  const employeeTable = new Table(stack, "EmployeeTable", {
+    fields: {
+      id: "string",
+      employeeId: "string",
+      lastName: "string",
+      phone: "string",
+      firstName: "string",
+    },
+    primaryIndex: { partitionKey: "employeeId", sortKey: "lastName" },
+  });
+
+  const importReportTable = new Table(stack, "ImportReportTable", {
+    fields: {
+      id: "string",
+      importId: "string",
+      validEmployeeCount: "number",
+      invalidEmployeeCount: "number",
+      errors: "string",
+    },
+    primaryIndex: { partitionKey: "importId" },
+  });
+
+  const importQueue = new Queue(stack, "EmployeeQueue", {
+    consumer: {
+      function: {
+        handler: "packages/functions/src/employeeQueueHandler.main",
+        bind: [employeeTable],
+      },
+    },
+  });
+
+  const reportQueue = new Queue(stack, "ReportQueue", {
+    consumer: {
+      function: {
+        handler: "packages/functions/src/storeReport.main",
+        bind: [importReportTable],
+      },
+    },
+  });
+
+  const api = new Api(stack, "EmployeeApi", {
+    defaults: {
+      function: {
+        bind: [importQueue, reportQueue],
+        timeout: 240,
+      },
+    },
+    routes: {
+      "POST /upload": {
+        function: {
+          handler: "packages/functions/src/validateEmployees.main",
+          bind: [importReportTable],
+        },
+      },
+      "POST /importReport": {
+        function: {
+          handler: "packages/functions/src/importReport.main",
+          bind: [importReportTable],
+        },
+      },
+    },
+  });
 
   stack.addOutputs({
     ApiEndpoint: api.url,
